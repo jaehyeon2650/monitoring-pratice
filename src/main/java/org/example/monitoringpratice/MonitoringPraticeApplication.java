868f75771574239538f8dea627a24cb3fecd2e30
wogus <ind07152@naver.com>
2025-09-25 22:15:59 +0900

    chore: 프로메테우스 관련 초기 설정

---
+++ src/main/java/org/example/monitoringpratice/MonitoringPraticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.monitoringpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/monitoringpratice/MonitoringPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.monitoringpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
