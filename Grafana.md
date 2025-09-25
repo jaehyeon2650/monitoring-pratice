7d1c5c427555226a75eb1022a018e438721bd9b0
wogus <ind07152@naver.com>
2025-09-25 23:59:37 +0900

    feat: Loki를 사용한 모니터링 설정 추가

---
+++ Grafana.md
@@ -0,0 +1,11 @@
+# 📖커넥션 연결하기
+## 1. 커넥션 추가하기
+![커넥션](./image/img.png)
+![커넥션](./image/img_1.png)
+add new data source 클릭
+## 2. 커넥션 연결하기
+![커넥션](./image/img_2.png)
+
+현재는 로컬에서 도커 컨테이너를 띄었기에 http://prometheus:9090로 설정
+## 3. Explore로 관측값 관찰하기
+![커넥션](./image/img_3.png)
--- src/main/java/org/example/monitoringpratice/TestController.java
+++ src/main/java/org/example/monitoringpratice/TestController.java
@@ -1,10 +1,12 @@
 package org.example.monitoringpratice;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
@@ -13,16 +15,19 @@ public class TestController {
 
     @GetMapping("/")
     public String home() {
+        log.info("come home");
         return "home";
     }
 
     @GetMapping("/home2")
     public String home2() {
+        log.info("come home2");
         return "home2";
     }
 
     @PostMapping("/member")
     public String member() {
+        log.info("come member");
         Member member = new Member("test");
         memberRepository.save(member);
 
